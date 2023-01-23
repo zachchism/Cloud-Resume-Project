410c33011bdc1f58b1e3da47b8f6639558793fa6
zachchism <48837572+zachchism@users.noreply.github.com>
2023-01-23 10:01:37 -0500

    Static Folder add
    Added Static Folder back for static website upload

---
+++ Static/script.js
@@ -0,0 +1,37 @@
+// api url
+const api_url =
+	"https://zachchismapp.azurewebsites.net/api/VisitIncriment";
+
+function returnCounter(){
+getapi(api_url).then(apiresult =>{
+let CounterInt = apiresult;
+
+let CounterintasString = `${CounterInt}`;
+
+//Return Message
+let CounterMessage = (`${CounterintasString}`);
+//alert(CounterMessage);
+const Counterelement = document.querySelector('.Counter');
+//document.getElementById("Counter").textContent = CounterMessage;
+document.getElementById("Counter").textContent = CounterInt;
+return CounterMessage;
+});
+}
+
+// Defining async function
+async function getapi(url) {
+	
+	// Storing response
+	const response = await fetch(url);
+	const names = await response.json();
+	
+	//var data = JSON.parse(response);
+	var data = JSON.parse(names);
+	//alert(data.amount);
+	var apiAmount = parseInt(data.amount);
+	//alert(apiAmount);
+	return(apiAmount);
+}
+
+returnCounter()
+
