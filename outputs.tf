547d0768eedd53c55ee2465bd6e5a754bf11859a
zachchism <48837572+zachchism@users.noreply.github.com>
2023-01-23 14:38:35 -0500

    Update outputs.tf
    Marked access key sensitive in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -29,5 +29,6 @@ output "storage_account_name" {
 }
 
 output "storage_account_key" {
-  value = azurerm_storage_account.sa.primary_access_key
+  value     = azurerm_storage_account.sa.primary_access_key
+  sensitive = true
 }
