5c1a9c37c89978e18ddd8669fb51420a0dba3491
zachchism <48837572+zachchism@users.noreply.github.com>
2023-01-24 19:52:55 -0500

    syntax change in cy test file

--- cypress/e2e/custom/test.cy.js
+++ cypress/e2e/custom/test.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 describe('My First Test', () => {
   it('finds the title "type"', () => {
-    cy.visit(Cypress.env('DEV_URL');
+    cy.visit(Cypress.env('DEV_URL'),
     cy.wait(2000);
     cy.get('#Counter').then(($counter) => {
       let value = parseInt($counter.text());
