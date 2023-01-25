a631f82ca5abecb58d023848be43b7aa7dbafb0b
zachchism <48837572+zachchism@users.noreply.github.com>
2023-01-24 19:48:18 -0500

    Syntax fix in cypress test file

--- cypress/e2e/custom/test.cy.js
+++ cypress/e2e/custom/test.cy.js
@@ -1,12 +1,12 @@
 /// <reference types="Cypress" />
 describe('My First Test', () => {
   it('finds the title "type"', () => {
-    cy.visit(Cypress.env('DEV_URL')
-    cy.wait(2000)
+    cy.visit(Cypress.env('DEV_URL');
+    cy.wait(2000);
     cy.get('#Counter').then(($counter) => {
       let value = parseInt($counter.text());
-      cy.visit(Cypress.env('DEV_URL')
-      cy.wait(2000)
+      cy.visit(Cypress.env('DEV_URL');
+      cy.wait(2000);
       cy.get('#Counter').then(($counter2) =>{
       let value2 = parseInt($counter2.text());
       expect(value2).to.eq(value + 1);
