349458a9741853204396de0a213dd325af8e3501
zachchism <48837572+zachchism@users.noreply.github.com>
2023-01-24 21:26:45 -0500

    Fixed value not found error

--- cypress/e2e/custom/test.cy.js
+++ cypress/e2e/custom/test.cy.js
@@ -2,15 +2,15 @@
 describe('My First Test', () => {  
   it('Gets value of counter', () => {
 	cy.visit(Cypress.env('DEV_URL')),  
-    cy.wait(5000)
+    cy.wait(2000)
     cy.get('#Counter').then(($counter) => {
       const value = parseInt($counter.text())
-      })
 	cy.visit(Cypress.env('DEV_URL')), 
-    cy.wait(5000)
+    cy.wait(2000)
     cy.get('#Counter').then(($counter2) => {
     const value2 = parseInt($counter2.text())
 	expect(value2).to.eq(value + 1)
       })
     })	
   })
+})
