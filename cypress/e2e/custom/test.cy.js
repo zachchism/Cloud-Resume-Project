1e6928f86b156ceed4fc04de39256695f56e31ba
zachchism <48837572+zachchism@users.noreply.github.com>
2023-01-24 21:02:47 -0500

    Update test.cy.js
    Increased counter time

--- cypress/e2e/custom/test.cy.js
+++ cypress/e2e/custom/test.cy.js
@@ -2,12 +2,12 @@
 describe('My First Test', () => {  
   it('Gets value of counter', () => {
 	cy.visit(Cypress.env('DEV_URL')),  
-    cy.wait(2000)
+    cy.wait(5000)
     cy.get('#Counter').then(($counter) => {
       let value = parseInt($counter.text());
       })
 	cy.visit(Cypress.env('DEV_URL')), 
-    cy.wait(2000)
+    cy.wait(5000)
     cy.get('#Counter').then(($counter2) => {
     let value2 = parseInt($counter2.text())
 	expect(value2).to.eq(value + 1)
