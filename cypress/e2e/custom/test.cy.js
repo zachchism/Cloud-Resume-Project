6fa0781738bbc2b2949e90360e1cf65041ad749a
zachchism <48837572+zachchism@users.noreply.github.com>
2023-01-26 12:10:00 -0500

    Added wait timers

--- cypress/e2e/custom/test.cy.js
+++ cypress/e2e/custom/test.cy.js
@@ -7,10 +7,12 @@ describe('My First Test', () => {
 	cy.wait(5000)	
     cy.get('#Counter').then(($counter) => {
       const value = parseInt($counter.text())
+	cy.wait(5000)
 	cy.visit(Cypress.env('DEV_URL')), 
     cy.wait(5000)
     cy.get('#Counter').then(($counter2) => {
     const value2 = parseInt($counter2.text())
+	cy.wait(5000)
 	expect(value2).to.eq(value + 1)
       })
     })	
