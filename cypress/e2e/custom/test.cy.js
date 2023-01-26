0cf00f28be0b3391a684f193c907b1764ebf7530
zachchism <48837572+zachchism@users.noreply.github.com>
2023-01-26 13:01:14 -0500

    Cypress test now waits properly
    Intercepts api calls instead of static wait times

--- cypress/e2e/custom/test.cy.js
+++ cypress/e2e/custom/test.cy.js
@@ -1,20 +1,24 @@
 /// <reference types="Cypress" />
 describe('WebApp Function Test', () => {  
   it('Gets value of counter', () => {
-	cy.visit(Cypress.env('DEV_URL')),  
-    cy.wait(2000)
-	cy.visit(Cypress.env('DEV_URL')),
-	cy.wait(5000)	
+	cy.visit(Cypress.env('DEV_URL')), 
+	cy.intercept('GET', 'webapp').as('counterRequest')
+    cy.wait('@counterRequest').then(($counter) => {
+		expect($counter).to.not.be.null
     cy.get('#Counter').then(($counter) => {
       const value = parseInt($counter.text())
-	cy.wait(5000)
+
 	cy.visit(Cypress.env('DEV_URL')), 
-    cy.wait(5000)
+	cy.intercept('GET', 'webapp').as('counterRequest')
+    cy.wait('@counterRequest').then(($counter2) => {
+		expect($counter2).to.not.be.null
     cy.get('#Counter').then(($counter2) => {
-    const value2 = parseInt($counter2.text())
-	cy.wait(5000)
+      const value2 = parseInt($counter2.text())
+	  
 	expect(value2).to.eq(value + 1)
       })
     })	
   })
+})
+})
 })
