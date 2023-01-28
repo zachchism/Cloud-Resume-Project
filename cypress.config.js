0ba7164131cb8e15d83d1359e206b7c9e69395f0
zachchism <48837572+zachchism@users.noreply.github.com>
2023-01-28 14:20:39 -0500

    moved requesttimeout

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+	requestTimeout: 10000,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-	  requestTimeout: 10000
+	 
     },
   },
 });
